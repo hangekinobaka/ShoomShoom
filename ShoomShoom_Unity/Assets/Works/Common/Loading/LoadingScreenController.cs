c2dc9550e41c016d09c37b629649b9b6a157a306
SleepyCat <hangekinobaka@gmail.com>
2023-01-23 22:46:41 +0900

    Add init camera script

---
+++ ShoomShoom_Unity/Assets/Works/Common/Loading/LoadingScreenController.cs
@@ -0,0 +1,41 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class LoadingScreenController : Singleton<LoadingScreenController>
+{
+    [SerializeField] CanvasGroup _canvasGroup;
+    [SerializeField] float _defaultDuration = .3f;
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void Show()
+    {
+        _canvasGroup.alpha = 1;
+    }
+
+    public void Hide()
+    {
+        _canvasGroup.alpha = 0;
+    }
+
+    public void FadeIn()
+    {
+        FadeIn(_defaultDuration);
+    }
+    public void FadeIn(float duration)
+    {
+        _canvasGroup.DOFade(1, duration);
+    }
+
+    public void FadeOut()
+    {
+        FadeOut(_defaultDuration);
+    }
+    public void FadeOut(float duration)
+    {
+        _canvasGroup.DOFade(0, duration);
+    }
+}
