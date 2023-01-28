24a90f374e9b4707d0e2ecbc41eb6ecce49a912c
SleepyCat <hangekinobaka@gmail.com>
2023-01-28 17:51:20 +0900

    Rewritethe ParallaxParts scripts

--- ShoomShoom_Unity/Assets/Works/Common/Camera/Parallax/Part/ParallaxParts.cs
+++ ShoomShoom_Unity/Assets/Works/Common/Camera/Parallax/Part/ParallaxParts.cs
@@ -1,112 +1,21 @@
-using UniRx;
 using UnityEngine;
 
 /// <summary>
-/// This part's multiplier is based on anoter layer
+/// ParallaxParts will only modify the localPosition
 /// </summary>
-public class ParallaxParts : ParallaxPartsBase
+public class ParallaxParts : ParallaxPartsRangeBase
 {
-    [Header("Activate Point")]
-    [SerializeField] bool _hasActivePoint = true;
-    [ConditionalDisplay("_hasActivePoint", true)]
-    [SerializeField] Transform _lActivePoint;
-    [ConditionalDisplay("_hasActivePoint", true)]
-    [SerializeField] Transform _rActivePoint;
-
-    Vector3 _rangeStartPos;
-    Vector3 _rangeEndPos;
-
-    float _lCamStartX;
-    float _rCamStartX;
-
-    CameraRangeTester _rangeTester;
-    bool _inRangeOnce = false;
+    [Header("Ranges")]
+    [SerializeField] ParallaxPartsRangeStruct[] _rangeStructs;
 
     override protected void Start()
     {
         base.Start();
-        if (_hasActivePoint)
+        if (_rangeStructs.Length > 0)
         {
-            InitRangePos();
-            InitRangeTester();
+            InitRange(_rangeStructs);
         }
 
     }
 
-    void InitRangePos()
-    {
-        _lCamStartX = _lActivePoint.position.x > _initStartCameraPos.x ? _lActivePoint.position.x : _initStartCameraPos.x;
-        _rCamStartX = _rActivePoint.position.x > _initStartCameraPos.x ? _rActivePoint.position.x : _initStartCameraPos.x;
-
-        _rangeStartPos = _startPos;
-        _rangeEndPos = _startPos;
-
-        _rangeEndPos.x += Mathf.Min(
-                Mathf.Abs(_rActivePoint.position.x - _lActivePoint.position.x),
-                Mathf.Abs(_rActivePoint.position.x - _initStartCameraPos.x))
-            * _plusMultiplier;
-
-    }
-
-    void UpdateRangeIn(RangeState state)
-    {
-        if (!_inRangeOnce)
-        {
-            _inRangeOnce = true;
-            _startCameraPos = _cameraTransform.position;
-            if (state == RangeState.LeftIn)
-            {
-                _startPos = _rangeStartPos;
-                _startCameraPos.x = _lCamStartX;
-            }
-            else
-            {
-                _startPos = _rangeEndPos;
-                _startCameraPos.x = _rCamStartX;
-            }
-            _startPos.y = transform.localPosition.y;
-        }
-
-        _curPlusMultiplier = _plusMultiplier;
-    }
-
-    void UpdateRangeOut(RangeState state)
-    {
-        _startCameraPos = _cameraTransform.position;
-        if (state == RangeState.LeftOut)
-        {
-            _startPos = _rangeStartPos;
-            _startCameraPos.x = _lCamStartX;
-        }
-        else
-        {
-            _startPos = _rangeEndPos;
-            _startCameraPos.x = _rCamStartX;
-        }
-        _startPos.y = transform.localPosition.y;
-        _curPlusMultiplier = 0;
-    }
-
-    void InitRangeTester()
-    {
-        _rangeTester = CameraRangeManager.Instance.AddRangeTester(_lActivePoint, _rActivePoint);
-
-        _rangeTester.CurRangeState.State.Subscribe(state =>
-        {
-            switch (state)
-
-            {
-                case RangeState.LeftIn:
-                case RangeState.RightIn:
-                    UpdateRangeIn(state);
-                    break;
-                case RangeState.LeftOut:
-                case RangeState.RightOut:
-                    UpdateRangeOut(state);
-                    break;
-                default:
-                    break;
-            }
-        }).AddTo(this);
-    }
 }
--- ShoomShoom_Unity/Assets/Works/Common/Camera/Parallax/Part/ParallaxPartsBase.cs
+++ ShoomShoom_Unity/Assets/Works/Common/Camera/Parallax/Part/ParallaxPartsBase.cs
@@ -3,9 +3,9 @@
 
 public class ParallaxPartsBase : MonoBehaviour
 {
-    [Header("Local")]
-    [SerializeField] protected float _plusMultiplier = 0.0f;
-    protected float _curPlusMultiplier = 0.0f;
+    [Header("Base")]
+    [SerializeField] protected float _plusMultiplierX = 0.0f;
+    [SerializeField] protected float _plusMultiplierY = 0.0f;
 
     [Header("Limits")]
     [SerializeField] bool _hasLimitY = false;
@@ -15,30 +15,24 @@ public class ParallaxPartsBase : MonoBehaviour
     protected Transform _cameraTransform;
 
     protected Vector3 _startCameraPos;
-    protected Vector3 _initStartCameraPos;
     protected Vector3 _startPos;
-    protected Vector3 _initStartPos;
     protected Vector3 _plusPos = Vector3.zero;
 
     virtual protected void Start()
     {
         _cameraTransform = Camera.main.transform;
         _startCameraPos = _cameraTransform.position;
-        _initStartCameraPos = _startCameraPos;
         _startPos = transform.localPosition;
-        _initStartPos = _startPos;
-
-        _curPlusMultiplier = _plusMultiplier;
-
     }
 
     private void LateUpdate()
     {
         // Calculate the plus pos(with the _plusMultiplier)
         Vector3 plusDelta = _cameraTransform.position - _startCameraPos;
-        _plusPos.x = plusDelta.x * _curPlusMultiplier;
-        _plusPos.y = plusDelta.y * _plusMultiplier;
+        _plusPos.x = plusDelta.x * _plusMultiplierX;
+        _plusPos.y = plusDelta.y * _plusMultiplierY;
 
+        // Limit the Y axis
         if (_hasLimitY)
         {
             if (Mathf.Abs(_plusPos.y) > _maxDeltaY)
