637838af236dd7ce5a868ccaec51da5cb5aa93c3
SleepyCat <hangekinobaka@gmail.com>
2023-03-09 22:35:25 +0900

    Add enemy stove anim and aim action.

--- ShoomShoom_Unity/Assets/Works/Common/Enemy/SpineAnimationController_Enemy.cs
+++ ShoomShoom_Unity/Assets/Works/Common/Enemy/SpineAnimationController_Enemy.cs
@@ -1,4 +1,6 @@
 ﻿using Spine;
+using Spine.Unity;
+using System.Collections;
 using UniRx;
 using UnityEngine;
 
@@ -8,14 +10,23 @@ namespace SleepySpine
     {
         const int MAIN_TRACK = 0;
         const int SECONDARY_TRACK = 1;
+        const int ATTACK_TRACK = 2;
 
         [SerializeField] protected EnemyController _enemyController;
 
         TrackEntry _trackMain => _skeletonAnimation.GetCurrentEntry(MAIN_TRACK);
         TrackEntry _trackSecondary => _skeletonAnimation.GetCurrentEntry(SECONDARY_TRACK);
 
-        private void Start()
+        protected virtual void Start()
         {
+            // Register controller events
+            _enemyController.OnAttack += Attack;
+            _enemyController.OnDistanceAttackStart += AimAndShoot;
+            _enemyController.OnDistanceAttackStop += StopAimAndShoot;
+
+            // Get necessary bones
+            _aimBone = _skeletonAnimation.Skeleton.FindBone(_aimBoneName);
+
             _enemyController.CurEnemyState.State.Subscribe(state =>
             {
                 switch (state)
@@ -41,5 +52,61 @@ namespace SleepySpine
                 }
             });
         }
+
+        private void OnDisable()
+        {
+            _enemyController.OnAttack -= Attack;
+            _enemyController.OnDistanceAttackStart -= AimAndShoot;
+            _enemyController.OnDistanceAttackStop -= StopAimAndShoot;
+        }
+
+        #region Attack
+        public void Attack()
+        {
+        }
+        #endregion
+
+        #region aim and shoot(Distance Attack)
+        [Header("Aim andd shoot")]
+        [SpineBone(dataField: "_skeletonAnimation")]
+        [SerializeField] string _aimBoneName;
+        Bone _aimBone;
+
+        bool _isAimingAndShooting = false;
+        Coroutine AimCoroutine;
+
+        public void AimAndShoot()
+        {
+            // Play aim anim
+            _spineAnimationState.SetAnimation(ATTACK_TRACK, "aim", false);
+
+            // Update
+            _isAimingAndShooting = true;
+            AimCoroutine = StartCoroutine(UpdateAim());
+        }
+        public void StopAimAndShoot()
+        {
+            // Stop Play aim anim
+            _spineAnimationState.AddEmptyAnimation(SECONDARY_TRACK, .1f, .1f);
+
+            // Update
+            _isAimingAndShooting = false;
+            AimCoroutine = null;
+        }
+
+        IEnumerator UpdateAim()
+        {
+            while (_isAimingAndShooting)
+            {
+                // Aim at the target pos
+                Vector3 skeletonSpacePoint = transform.InverseTransformPoint(_enemyController.AttackTargetPos);
+                skeletonSpacePoint.x *= _skeletonAnimation.Skeleton.ScaleX;
+                skeletonSpacePoint.y *= _skeletonAnimation.Skeleton.ScaleY;
+                _aimBone.SetLocalPosition(skeletonSpacePoint);
+
+                yield return null;
+            }
+        }
+        #endregion
     }
 }
--- ShoomShoom_Unity/Assets/Works/Common/Enemy/SpineAnimationController_RobotDog.cs
+++ ShoomShoom_Unity/Assets/Works/Common/Enemy/SpineAnimationController_RobotDog.cs
@@ -3,7 +3,14 @@ namespace SleepySpine
 {
     public class SpineAnimationController_RobotDog : SpineAnimationController_Enemy
     {
+        const int STOVE_TRACK = 10;
+        protected override void Start()
+        {
+            base.Start();
 
+            // Add fire stove anim
+            _spineAnimationState.SetAnimation(STOVE_TRACK, "stove-fire", true);
+        }
 
     }
 }
